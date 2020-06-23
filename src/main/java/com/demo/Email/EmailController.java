ce92230751bbbac0d2ba343e1d59b776e413658a
jnzheng <jnzheng@jnzhengs-MacBook-Pro.local>
2020-06-23 08:11:15 +0800

    Modify controller

--- src/main/java/com/demo/Email/EmailController.java
+++ src/main/java/com/demo/Email/EmailController.java
@@ -1,15 +1,20 @@
 package com.demo.Email;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/email")
+@SpringBootApplication
 public class EmailController {
-    @GetMapping
-    public String getUserEmail(@RequestParam(defaultValue = "") Long userId) {
-        return String.format("%s@rest.local", userId);
+
+    @RestController
+    @RequestMapping("/emails")
+    public class ClientAController {
+
+        @RequestMapping("/{emailId}")
+        public String getBook(@PathVariable("emailId") String emailId) {
+            return "This is email id: " + emailId;
+        }
     }
 }
