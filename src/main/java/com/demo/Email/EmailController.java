141accd66c893f53ce973116f9d07863e05bfc76
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-06-23 18:02:32 +0800

    pretty printing message

--- src/main/java/com/demo/Email/EmailController.java
+++ src/main/java/com/demo/Email/EmailController.java
@@ -1,19 +1,28 @@
 package com.demo.Email;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class EmailController {
+
+    @Autowired
+    Environment environment;
 
     @RestController
     public class ClientAController {
 
         @RequestMapping("/emails/{emailId}")
-        public String getBook(@PathVariable("emailId") String emailId) {
-            System.out.print("****** Access Email ******\n");
+        public String getBook(@PathVariable("emailId") String emailId) throws UnknownHostException {
+
+            String port = environment.getProperty("server.port");
+            System.out.print(String.format("****************** Access Email from port %s ******************\n", port));
             return emailId + "@rest.local";
         }
 
